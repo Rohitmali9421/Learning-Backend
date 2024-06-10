88479f28c917a6db4eb0574987cd6576212a4499
Rohit Mali <rohitsmali9421@gmail.com>
2024-06-10 12:07:57 +0530

    rest api

---
+++ Project01/index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const users = require("./MOCK_DATA.json");
+const app = express();
+
+app.get("/users", (req, res) => {
+  const html = `
+        <ul>
+            ${users.map((user) => `<li>${user.first_name}</li>`).join("")}
+        </ul>
+    `;
+  res.send(html);
+});
+
+app.get("/api/users", (req, res) => {
+  return res.json(users);
+});
+app
+  .route("/api/users/:id")
+  .get((req, res) => {
+    const id = Number(req.params.id);
+    const user = users.find((user) => user.id === id);
+    return res.json(user);
+  })
+  .patch((req, res) => {
+    //TODO:
+    return res.json({ status: "pending" });
+  })
+  .delete((req, res) => {
+    //TODO:
+    return res.json({ status: "pending" });
+  });
+  
+app.post("/api/users", (req, res) => {
+  //TODO:
+  return res.json({ status: "pending" });
+});
+
+app.listen(8000, () => {
+  console.log("server started");
+});
