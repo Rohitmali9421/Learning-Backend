045a876a72ed957b4d146c3574dbd2cccf46f217
Rohit Mali <rohitsmali9421@gmail.com>
2024-06-11 19:28:09 +0530

    ,

--- Project01/index.js
+++ Project01/index.js
@@ -1,38 +1,64 @@
 const express = require("express");
 const users = require("./MOCK_DATA.json");
+const fs = require("fs");
 const app = express();
-
-app.get("/users", (req, res) => {
-  const html = `
-        <ul>
-            ${users.map((user) => `<li>${user.first_name}</li>`).join("")}
-        </ul>
-    `;
-  res.send(html);
-});
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 app.get("/api/users", (req, res) => {
   return res.json(users);
 });
-app
-  .route("/api/users/:id")
+
+app.route("/api/users/:id")
   .get((req, res) => {
     const id = Number(req.params.id);
     const user = users.find((user) => user.id === id);
     return res.json(user);
   })
   .patch((req, res) => {
-    //TODO:
-    return res.json({ status: "pending" });
+    const id = Number(req.params.id);
+    const index = users.findIndex(user => user.id === id);
+    if (index !== -1) {
+      const updatedUser = { ...users[index], ...req.body };
+      users[index] = updatedUser;
+      fs.writeFile("./MOCK_DATA.json", JSON.stringify(users, null, 2), (err) => {
+        if (err) {
+          return res.status(500).json({ status: "error", message: "Failed to update user data" });
+        }
+        return res.json({ status: "success", user: updatedUser });
+      });
+    } else {
+      return res.status(404).json({ status: "error", message: "User not found" });
+    }
   })
   .delete((req, res) => {
-    //TODO:
-    return res.json({ status: "pending" });
+    const id = Number(req.params.id);
+    const index = users.findIndex(user => user.id === id);
+    if (index !== -1) {
+      users.splice(index, 1);
+      fs.writeFile("./MOCK_DATA.json", JSON.stringify(users, null, 2), (err) => {
+        if (err) {
+          return res.status(500).json({ status: "error", message: "Failed to delete user" });
+        }
+        return res.json({ status: "success" });
+      });
+    } else {
+      return res.status(404).json({ status: "error", message: "User not found" });
+    }
   });
-  
+
 app.post("/api/users", (req, res) => {
-  //TODO:
-  return res.json({ status: "pending" });
+  const body = req.body;
+  users.push({ ...body, id: users.length + 1 });
+  fs.writeFile("./MOCK_DATA.json", JSON.stringify(users, null, 2), (err) => {
+    if (err) {
+      return res.status(500).json({ status: "error", message: "Failed to add user" });
+    }
+    return res.json({
+      status: "success",
+      id: users.length
+    });
+  });
 });
 
 app.listen(8000, () => {
