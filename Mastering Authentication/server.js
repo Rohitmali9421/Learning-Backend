f387da2b1c37dda1bdbcf52f2bd58cd910657f78
Rohitmali9421 <rohitsmali9421@gmail.com>
2026-02-18 22:40:58 +0530

    Learning 2 Token Authentication

--- Mastering Authentication/server.js
+++ Mastering Authentication/server.js
@@ -1,12 +1,21 @@
 import express from "express"
 import dotenv from "dotenv";
 import connectDB from "./config/connection.js";
+import cors from "cors"
+import cookieParser from "cookie-parser";
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 const app = express();
 
 connectDB();
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173", 
+    credentials: true,
+  })
+);
 app.get("/",(req,res)=>{
     res.send("hello");
 })
